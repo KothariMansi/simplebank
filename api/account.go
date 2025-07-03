afc48cf5de4495c941c7d58e0eaa3fb3922668a0
SHREE <mansikothari46@gmail.com>
2025-07-03 18:00:26 +0530

    feat: add get account endpoint and implement request handling

--- api/account.go
+++ api/account.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	db "github.com/KothariMansi/simplebank/db/sqlc"
@@ -31,6 +32,30 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	ctx.JSON(http.StatusOK, account)
+
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	arg := req.ID
+	account, err := server.store.GetAccount(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 	ctx.JSON(http.StatusOK, account)
 
 }
