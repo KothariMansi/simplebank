961c326068b9c3994ceffd262af39b756103c6e2
SHREE <mansikothari46@gmail.com>
2025-07-04 11:26:18 +0530

    feat: add list account endpoint and implement pagination handling; add emit empty slice in sqlc

--- api/account.go
+++ api/account.go
@@ -57,5 +57,28 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountRequest struct {
+	PageId   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=1,max=20"`
+}
+
+func (server *Server) listAccount(ctx *gin.Context) {
+	var req listAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	arg := db.ListAccountParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageId - 1) * req.PageSize,
+	}
+	accounts, err := server.store.ListAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, accounts)
 
 }
