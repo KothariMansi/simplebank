d90d2fd281858b04e7cb635de9b885dfb9f51e8c
SHREE <mansikothari46@gmail.com>
2025-07-14 08:55:41 +0530

    feat: add mock generation for store interface; implement account API tests and update SQLC version to 1.29.0

--- api/server.go
+++ api/server.go
@@ -7,12 +7,12 @@ import (
 
 // Server serve HTTP request for our banking service
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 // New Server create a new HTTP server and setup routing.
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
