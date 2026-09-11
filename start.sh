7c8d8e4b0411841268fa3e365efcd28a6a008af3
KothariMansi <mansikothari46@gmail.com>
2026-09-11 18:51:53 +0530

    feat: add Dockerfile, docker-compose, and start script for multi-stage build and database migration

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+echo "run db migration"
+
+echo "DB_SOURCE is: $DB_SOURCE"
+
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+
+echo "start the app"
+
+exec "$@"
